9e61181bac4e03f8ab74d15573f24f405af033b2
yveskalume <yvkalume@gmail.com>
2025-05-24 00:05:57 +0200

    Refactor: Configure Maven Publishing

--- qrpainter/build.gradle.kts
+++ qrpainter/build.gradle.kts
@@ -1,13 +1,15 @@
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
-    id("maven-publish")
+    alias(libs.plugins.mavenPublish)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "com.yveskalume.compose.qrpainter"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
@@ -35,9 +37,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
 }
 
 dependencies {
@@ -48,16 +47,36 @@ dependencies {
     implementation(libs.ui)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "com.yveskalume"
-            artifactId = "compose-qrpainter"
-            version = "0.0.1"
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 
-            afterEvaluate {
-                from(components["release"])
+    coordinates("dev.yveskalume", "compose-qrpainter", "1.0.0-alpha01")
+
+    pom {
+        name.set("ComposeQRPainter")
+        description.set("Generate QR codes in Jetpack Compose")
+        inceptionYear.set("2023")
+        url.set("https://github.com/yveskalume/compose-qrpainter")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("yveskalume")
+                name.set("Yves Kalume")
+                url.set("https://github.com/yveskalume")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/yveskalume/compose-qrpainter")
+            connection.set("scm:git:git://github.com/yveskalume/compose-qrpainter.git")
+            developerConnection.set("scm:git:ssh://git@github.com/yveskalume/compose-qrpainter.git")
         }
     }
 }
